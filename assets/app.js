29945653824d119bf30dab6b83791889df5b41f2
Sasha Patsel <sashapatsel@Sashas-MacBook-Pro.local>
2018-02-01 12:46:26 -0800

    gifs are now pausable

--- assets/app.js
+++ assets/app.js
@@ -35,49 +35,41 @@ newButtons()
             var rating = results[i].rating;
 
             var p = $("<p>").text("Rating: " + rating);
-            console.log(results[i].images.fixed_height_small_still.url);
+            console.log(results[i].images.fixed_height_still.url);
             var chosenImage = $("<img>");
             chosenImage.attr("src", results[i].images.fixed_height.url);
-            chosenImage.data("still", results[i].images.fixed_height_small_still.url)
-            chosenImage.data("animate", results[i].images.fixed_height_small.url)
-            chosenImage.data("state", "still")
+            chosenImage.attr("data-still", results[i].images.fixed_height_still.url)
+            chosenImage.attr("data-animate", results[i].images.fixed_height.url)
+            chosenImage.attr("data-state", "still")
             chosenImage.addClass("gif")
             gifDiv.prepend(p);
             gifDiv.prepend(chosenImage);
 
             $(".more-gifs").prepend(gifDiv);
-            //give this console.log a link and you're set
-        // var state = $(results[1]).attr("data-state");
-        // // console.log(state);
-
-        // var dataAnimate = $(results[1]).data("animate");
-        // var dataStill = $(results[1]).data("still");
-        // console.log(dataAnimate);
-        
 
           }
         });
       }
 
-    $("<img>").on("click", function() {
-
+    $(document).on("click",".gif", function() {
+ 				console.log("hi");
         var state = $(this).attr("data-state");
         // console.log(state);
 
-        var dataAnimate = $(this).data("animate");
-        var dataStill = $(this).data("still");
+        var dataAnimate = $(this).attr("data-animate");
+        var dataStill = $(this).attr("data-still");
        
 
         if (state === "still") {
         	 console.log(dataAnimate);
         console.log(dataStill);
           $(this).attr("src", dataAnimate);
-          $(this).data("state", "animate");
+          $(this).attr("data-state", "animate");
           console.log(state);
         } else if (state === "animate") {
           $(this).attr("src", dataStill);
-          $(this).data("state", "still");
-          console.log("hi");
+          $(this).attr("data-state", "still");
+         
         }
 
     });
